5f24a4be6d80da864356ff92b3341b7a23709d8e
Diego Martín <martin.diego11@hotmail.com>
2016-03-01 10:47:42 +0100

    exportación de la base de datos

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^consultas/', include('consultas.urls')),
+    url(r'^consultas/', include('consultas.urls', namespace="consultas")),
     url(r'^admin/', admin.site.urls),
 ]
