b814a70aac6ac646f377e5016ef049a57e3d00d0
Diego Martín <martin.diego11@hotmail.com>
2016-03-01 11:08:43 +0100

    exportación de la base de datos

--- consultas/urls.py
+++ consultas/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 
 from . import views
 
 app_name = 'consultas'
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^info/(?P<empresa_cif>.+)$', views.info, name='info'),
-]
+urlpatterns = patterns(
+    '',
+    url(r'^$', consultas.views.index, name='index'),
+    url(r'^info/(?P<empresa_cif>.+)$', consultas.views.info, name='info'),
+)
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from . import views
 
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', mysite.views.index, name='index'),
     url(r'^consultas/', include('consultas.urls', namespace='consultas')),
     url(r'^admin/', admin.site.urls),
-]
+)
+
+# [
+#     url(r'^$', views.index, name='index'),
+#     url(r'^consultas/', include('consultas.urls', namespace='consultas')),
+#     url(r'^admin/', admin.site.urls),
+# ]
